7a1d519b95193c7b38438e985ba27ecfb476e81e
givemefive9 <195841317+givemefive9@users.noreply.github.com>
2025-04-24 16:22:47 +0800

    feat: update grid demo, with max-content, minmax().

--- examples/css/src/grid_layout/index.tsx
+++ examples/css/src/grid_layout/index.tsx
@@ -1,20 +1,45 @@
 import { root } from "@lynx-js/react";
+import { useCallback, useEffect, useState } from "@lynx-js/react";
+import arrow from "./assets/arrow.png";
+import lynxLogo from "./assets/lynx-logo.png";
+import reactLynxLogo from "./assets/react-logo.png";
 
 import "./index.scss";
 const GridComponent = () => {
+  const [alterLogo, setAlterLogo] = useState(false);
+
+  useEffect(() => {
+    console.info("Hello, ReactLynx");
+  }, []);
+
+  const onTap = useCallback(() => {
+    "background-only";
+    setAlterLogo(!alterLogo);
+  }, [alterLogo]);
+
   return (
-    <view style={{ width: "100%", height: "100%", flexDirection: "column" }}>
-      <text className="header__title colorWidth">Grid layout</text>
-      <view className="container">
-        <view className="item1"></view>
-        <view className="item2"></view>
-        <view className="item3"></view>
-        <view className="item4"></view>
-        <view className="item5"></view>
-        <view className="item6"></view>
-        <view className="item7"></view>
+    <page>
+      <view className="Background" />
+      <view className="Container">
+        <view className="GridContainer" style={{ gridTemplateColumns: "20% max-content minmax(50px, max-content)" }}>
+          <view className="Logo" bindtap={onTap}>
+            {alterLogo
+              ? <image src={reactLynxLogo} className="Logo--react" />
+              : <image src={lynxLogo} className="Logo--lynx" />}
+          </view>
+          <text className="Title" style={{ alignSelf: "center" }}>No Wrap</text>
+          <text className="Subtitle">min-width:50px, will fit the container</text>
+        </view>
+        <view className="Content">
+          <image src={arrow} className="Arrow" />
+          <text className="Description">Tap the logo and have fun!</text>
+          <text className="Hint">
+            Edit<text style={{ fontStyle: "italic" }}>{" src/index.tsx "}</text>
+            to see updates!
+          </text>
+        </view>
       </view>
-    </view>
+    </page>
   );
 };
 root.render(<GridComponent />);
