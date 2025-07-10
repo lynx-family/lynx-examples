fbd23414f7c2a0d8e0c0c1f9e6bdc609220530c7
BitterGourd <91231822+gaoachao@users.noreply.github.com>
2025-07-10 16:21:19 +0800

    feat: add TanStack Router example

---
+++ examples/tanstack-router/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+
+import { Button } from "../components/button.js";
+
+// @ts-ignore
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  const navigate = useNavigate();
+
+  return (
+    <view>
+      <text>Current Page: Home</text>
+      <view style={{ height: "8px" }} />
+      <Button bindtap={() => navigate({ to: "/a" })}>
+        <text>Go to Page A</text>
+      </Button>
+    </view>
+  );
+}
