4f96ce823be5870c484034f793baffc3ff981e39
huzhanbo1996 <20810265+huzhanbo1996@users.noreply.github.com>
2025-08-28 16:53:22 +0800

    fetch support chunk demo (#111)
    * fetch support chunk demo
    
    * remove ts-ignore
    
    * remove globalthis
    
    * Create serious-points-share.md
    
    Signed-off-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
    Co-authored-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>

--- examples/fetch/index.tsx
+++ examples/fetch/index.tsx
@@ -23,10 +23,39 @@ const App = () => {
     }
   };
 
+  async function streamToArrayBuffer(stream: ReadableStream) {
+    const reader = stream.getReader();
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      } else {
+        const text = TextCodecHelper.decode(value);
+        setData(text);
+      }
+    }
+  }
+
+  const handleButtonClickStreaming = async () => {
+    setError(null);
+
+    console.log("fetch streaming");
+    lynx.fetch("https://e3e0932a-9522-4aec-aec9-12cba94b0c7f.mock.pstmn.io/chunk", {
+      lynxExtension: { "useStreaming": true },
+    }).then((response) => {
+      streamToArrayBuffer(response.body);
+    }).catch((err: Error) => {
+      setError(err.message);
+    });
+  };
+
   return (
     <view className="container">
       <view className="btn" bindtap={handleButtonClick}>
         <text>fetch</text>
+      </view>
+      <view className="btn" bindtap={handleButtonClickStreaming}>
+        <text>fetchChunk</text>
       </view>
       <scroll-view className="box" scroll-orientation="vertical">
         {error && <text className="error">Error: {error}</text>}
