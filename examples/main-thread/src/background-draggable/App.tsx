8a2724db6bf08c76658125c2edf646c68c1f0253
Sherry-hue <37186915+Sherry-hue@users.noreply.github.com>
2025-09-01 16:31:50 +0800

    chore(web): main-thread remove global-bind usage, preview this example on the web in the future

--- examples/main-thread/src/background-draggable/App.tsx
+++ examples/main-thread/src/background-draggable/App.tsx
@@ -1,6 +1,18 @@
+import { useState } from "@lynx-js/react";
+import type { ScrollEvent } from "@lynx-js/types";
 import { BackgroundDraggable } from "./BackgroundDraggable.jsx";
 
 export function App() {
+  const [posStyle, setPosStyle] = useState({ x: 0, y: 500 });
+  const onScroll = (event: ScrollEvent) => {
+    const detail = event.detail.scrollTop;
+    const newPos = {
+      x: 0,
+      y: 500 - detail,
+    };
+    setPosStyle(newPos);
+  };
+
   return (
     <view
       style={{
@@ -13,7 +25,7 @@ export function App() {
       <view style="display:linear;linear-direction:row;width:100%;height:100%">
         <scroll-view
           id="scroll"
-          bindscroll={() => {}}
+          bindscroll={onScroll}
           scroll-y
           style="display:linear;linear-direction:row;width:50%;height:100%"
         >
@@ -22,7 +34,7 @@ export function App() {
           <view style="background:yellow;width:100%;height:1000px" />
         </scroll-view>
         <view style="width:50%;height:100%;display:linear;linear-direction:row;">
-          <BackgroundDraggable size={100} />
+          <BackgroundDraggable size={100} posStyle={posStyle} />
         </view>
       </view>
     </view>
