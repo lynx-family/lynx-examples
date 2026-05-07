3dc58bd92489062055b51307b728858e6f24dd1a
luhengchang <luhengchang@bytedance.com>
2026-05-07 10:38:25 +0800

    feat: upgrade @lynx-js/types to 3.8.0

--- examples/refresh/src/base/index.tsx
+++ examples/refresh/src/base/index.tsx
@@ -30,45 +30,41 @@ const App = () => {
       }}
     >
       {
-        // @ts-expect-error type error
-
-
-          <refresh ref={refreshViewRef} style={{ width: "100%", height: "100%" }} bindstartrefresh={onStartRefresh}>
-            <refresh-header
-              style={{
-                width: "100%",
-                height: "50px",
-                position: "absolute",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "red",
-              }}
-            >
-              <text style={{ fontSize: "30px" }}>Refreshing...</text>
-            </refresh-header>
-            <list style={{ width: "100%", height: "100%" }} scroll-orientation="vertical" bounces={true}>
-              {items.map((item, index) => {
-                return (
-                  <list-item key={item + ""} item-key={item + ""}>
-                    <text
-                      style={{
-                        "height": "200px",
-                        width: "100%",
-                        fontSize: "100px",
-                        color: refreshCount % 2 === 0 ? "red" : "green",
-                        padding: "25px",
-                        textAlign: "center",
-                        border: "1px solid black",
-                      }}
-                    >
-                      {`item-${index}`}
-                    </text>
-                  </list-item>
-                );
-              })}
-            </list>
-          </refresh>
-
+        <refresh ref={refreshViewRef} style={{ width: "100%", height: "100%" }} bindstartrefresh={onStartRefresh}>
+          <refresh-header
+            style={{
+              width: "100%",
+              height: "50px",
+              position: "absolute",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "red",
+            }}
+          >
+            <text style={{ fontSize: "30px" }}>Refreshing...</text>
+          </refresh-header>
+          <list style={{ width: "100%", height: "100%" }} scroll-orientation="vertical" bounces={true}>
+            {items.map((item, index) => {
+              return (
+                <list-item key={item + ""} item-key={item + ""}>
+                  <text
+                    style={{
+                      "height": "200px",
+                      width: "100%",
+                      fontSize: "100px",
+                      color: refreshCount % 2 === 0 ? "red" : "green",
+                      padding: "25px",
+                      textAlign: "center",
+                      border: "1px solid black",
+                    }}
+                  >
+                    {`item-${index}`}
+                  </text>
+                </list-item>
+              );
+            })}
+          </list>
+        </refresh>
       }
     </view>
   );
