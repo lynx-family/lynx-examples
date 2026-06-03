6692ffae40467d8316197103c1d1f8b9191a4037
liubb0516 <liu1653617791@qq.com>
2026-06-03 20:17:27 +0800

    Add offset-path, offset-distance, offset-rotate example
    Add new css `offset-path`, `offset-distance`, `offset-rotate` example

---
+++ api/css/src/offset-distance/App.tsx
@@ -0,0 +1,11 @@
+import { root } from "@lynx-js/react";
+import "./index.scss";
+
+const OffsetDistance = () => {
+  return (
+    <view className="box">
+    </view>
+  );
+};
+
+root.render(<OffsetDistance />);
